21767ac2101a4ed8fe40c41a611ffdd7a6adb7a7
Luan Ferreira <luan.lnf94@gmail.com>
2024-12-02 19:57:35 -0300

    first commit

---
+++ exercicio/exercicio.js
@@ -0,0 +1,11 @@
+"use strict";
+function multiplica(a, b) {
+    return a * b;
+}
+function diOlá(name) {
+    return 'olá' + name;
+}
+const resultadoMultiplicacao = multiplica(2, 3);
+console.log(resultadoMultiplicacao);
+const saudacao = diOlá('Guilherme');
+console.log(saudacao);
---
+++ exercicio/exercicio.ts
@@ -0,0 +1,12 @@
+function multiplica(a: number, b: number): number {
+    return a * b;
+}
+function diOlá(name: string):string {
+    return 'olá' + name;
+}
+
+const resultadoMultiplicacao = multiplica(2, 3);
+console.log(resultadoMultiplicacao);
+
+const saudacao = diOlá('Guilherme');
+console.log(saudacao);
